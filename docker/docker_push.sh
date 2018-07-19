b7c21e6b436996461cd80056287874c3eed52909
JanedoeGVA <xavier.costa@unige.ch>
2018-07-19 15:06:23 +0200

    first commit

--- docker/docker_push.sh
+++ docker/docker_push.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-mv build/libs/cx.rest.datavatar.war docker/cx.rest.datavatar.war
-
 docker build ./docker/ --build-arg VCS_REF=`git rev-parse --short HEAD` -t janedoegva/nginx:$BRANCH
 
 docker push janedoegva/nginx:$BRANCH
