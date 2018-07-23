28d2b0858a7079e1f1d0853b36722a0d92062afc
JanedoeGVA <xavier.costa@unige.ch>
2018-07-23 14:58:20 +0200

    adding script

--- script.sh
+++ script.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ ! -f $DH_PARAM_OUT ]; then
+    echo "File not found!"
+    sudo openssl dhparam -out $DH_PARAM_OUT 2048
+else
+    echo "File found!"
+fi
+
 chmod a+w /etc/nginx/conf.d/nginx.conf
 
 sed -i "s|\${server_name}|$SERVER_NAME|" /etc/nginx/conf.d/nginx.conf
