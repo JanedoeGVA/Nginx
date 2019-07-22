38ea9ce613da6162b07dc6ce2643e266a8f5d0b0
JanedoeGVA <36301580+JanedoeGVA@users.noreply.github.com>
2019-07-22 21:52:23 +0200

    adding keycloak_api_url

--- script.sh
+++ script.sh
@@ -4,6 +4,7 @@ chmod a+w /etc/nginx/conf.d/nginx.conf
 
 sed -i "s|\${server_name}|$SERVER_NAME|" /etc/nginx/conf.d/nginx.conf
 sed -i "s|\${datavatar_api_url}|$DATAVATAR_API_URL|" /etc/nginx/conf.d/nginx.conf
+sed -i "s|\${keycloak_api_url}|$KEYCLOAK_API_URL|" /etc/nginx/conf.d/nginx.conf
 sed -i "s|\${fullchain_pem}|$FULLCHAIN_PEM|" /etc/nginx/conf.d/nginx.conf
 sed -i "s|\${privkey_pem}|$PRIVKEY_PEM|" /etc/nginx/conf.d/nginx.conf
 sed -i "s|\${dhparam_2048}|$DHPARAM|" /etc/nginx/conf.d/nginx.conf
