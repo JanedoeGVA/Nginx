60f13045e9a09d8240601b9d68dc8527acb60672
JanedoeGVA <36301580+JanedoeGVA@users.noreply.github.com>
2019-10-03 10:08:55 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,7 +4,6 @@ chmod a+w /etc/nginx/conf.d/nginx.conf
 
 sed -i "s|\${server_name}|$SERVER_NAME|" /etc/nginx/conf.d/nginx.conf
 sed -i "s|\${general_consent_api_url}|$GENERAL_CONSENT_API_URL|" /etc/nginx/conf.d/nginx.conf
-sed -i "s|\${portainer_url}|$PORTAINER_URL|" /etc/nginx/conf.d/nginx.conf
 sed -i "s|\${fullchain_pem}|$FULLCHAIN_PEM|" /etc/nginx/conf.d/nginx.conf
 sed -i "s|\${privkey_pem}|$PRIVKEY_PEM|" /etc/nginx/conf.d/nginx.conf
 sed -i "s|\${dhparam_2048}|$DHPARAM|" /etc/nginx/conf.d/nginx.conf
